7e968c1136090024883ae08edb0f889a92305939
nathan-renard <aimbire@proton.me>
2025-06-11 12:24:07 -0700

    the thing moves now, cool!

--- player.py
+++ player.py
@@ -18,6 +18,10 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -27,5 +31,7 @@ class Player(CircleShape):
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
+            self.move(dt)
         if keys[pygame.K_d]:
+            self.move(dt)
             self.rotate(dt)
