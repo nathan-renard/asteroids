355e2ef1a5683426f3052003a5cecad539796005
nathan-renard <aimbire@proton.me>
2025-06-11 12:09:19 -0700

    we have a player now, it's stuck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,8 +10,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    dt = 60  # Assuming a fixed delta time for simplicity
+    dt = 0  # Assuming a fixed delta time for simplicity
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
   
     running = True
     while running:
@@ -19,7 +21,8 @@ def main():
                 running = False
 
         pygame.Surface.fill(screen, (0, 0, 0))  
-        pygame.display.flip()
+        player.draw(screen)
+        pygame.display.flip()        
         dt = clock.tick() / 1000.0  # Convert milliseconds to seconds
 
 if __name__ == "__main__":    
